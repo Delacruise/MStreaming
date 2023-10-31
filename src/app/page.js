df062335664f8dc861d395296a15a532656e654e
Luke Daffue <25479590+Delacruise@users.noreply.github.com>
2023-10-31 16:10:18 +0400

    artist page

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,8 @@ import { useState, useEffect } from 'react';
 import axios, { isCancel, AxiosError } from 'axios';
 import Image from 'next/image';
 import PlaylistList from './components/playlist';
+import Artists from './components/artists';
+import Albums from './components/albums';
 import playListGet from '../app/api/playListGet/route';
 import { fromJSON } from 'postcss';
 
@@ -22,8 +24,12 @@ export default function Home() {
       <div className='h-fit bg-purple-600 p-4 pr-8 '>
         <PlaylistList />
       </div>
-      <div className='h-40 bg-yellow-600 p-4'>Artists</div>
-      <div className='h-40 bg-orange-600 p-4'>Albums</div>
+      <div className='h-fit bg-yellow-600 p-4 pr-8'>
+        <Artists />
+      </div>
+      <div className='h-fit bg-indigo-600 p-4 pr-8'>
+        <Albums />
+      </div>
     </main>
   );
 }
